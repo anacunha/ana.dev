7358b06e62fb9e620126f99865ab2c8e1ad9702a
Ana Cunha <1771610+anacunha@users.noreply.github.com>
2026-07-27 12:10:20 -0300

    Configure i18n routing and sitemap hreflang

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,27 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://ana.dev",
+  i18n: {
+    locales: ["en", "es", "pt"],
+    defaultLocale: "en",
+    routing: {
+      prefixDefaultLocale: false,
+    },
+  },
+  integrations: [
+    sitemap({
+      i18n: {
+        defaultLocale: "en",
+        locales: {
+          en: "en",
+          es: "es",
+          pt: "pt",
+        },
+      },
+    }),
+  ],
+});
